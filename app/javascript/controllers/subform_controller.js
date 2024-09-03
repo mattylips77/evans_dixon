7e9224392abcf84d53c68a6b7ee76064669ffb3e
Matt Lipschutz <matt@Matts-MacBook-Pro.local>
2024-09-02 23:21:09 -0400

    added subform overlay

--- app/javascript/controllers/subform_controller.js
+++ app/javascript/controllers/subform_controller.js
@@ -1,5 +1,29 @@
 // src/controllers/subform_controller.js
 import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
-    static targets = ["button", "savedForms"]
+    static targets = ["button", "savedForms", "overlay", "subform_body"];
+
+    initialize() {
+        console.log('stim test')
+    }
+
+    closeOverlay = (e) => {
+        this.overlayTarget.classList.add("hidden")
+    }
+
+    addForm = (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(formElement.action, {
+                method: 'POST',
+                headers: { "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute("content") },
+                body: formData,
+            });
+
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+
+        this.overlayTarget.classList.remove("hidden")
+    }
 }
